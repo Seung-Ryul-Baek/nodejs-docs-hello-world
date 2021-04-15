1afdd74dfaed532333049d20bfb134d683ddd83b
Seung-Ryul-Baek <64510368+Seung-Ryul-Baek@users.noreply.github.com>
2021-04-15 17:11:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello IU!");
-});
+});    
 
 const port = process.env.PORT || 3000;
 //const port = process.env.PORT || 1337;    // for azure devops
